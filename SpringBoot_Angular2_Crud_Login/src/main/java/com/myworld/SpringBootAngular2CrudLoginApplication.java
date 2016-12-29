3be0fd69404e6bf217c686cc610454707e10b8c9
Gyan <gyanqk@gmail.com>
2016-12-29 08:19:59 -0800

    first

---
+++ SpringBoot_Angular2_Crud_Login/src/main/java/com/myworld/SpringBootAngular2CrudLoginApplication.java
@@ -0,0 +1,12 @@
+package com.myworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngular2CrudLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngular2CrudLoginApplication.class, args);
+	}
+}
